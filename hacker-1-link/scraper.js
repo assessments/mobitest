4a9d5e4553fba1ed5a93baec0b2c52102a2b2275
Mark Macdonald <mark.t.macdonald@googlemail.com>
2014-10-10 15:31:22 +0200

    initial structure for web scraper.

---
+++ hacker-1-link/scraper.js
@@ -0,0 +1,85 @@
+
+/*
+	- todo: HTTPS?
+	- todo: dynamically generated links (e.g. PhantomJS)
+*/
+
+var request = require('request');
+var cheerio = require('cheerio');
+var url = require('url')
+
+function Scraper() {
+
+	this.url = '';
+
+	this.body = '';
+
+	this.links = [];
+
+	this.process = function () {
+		this.parse()
+		scraper.analyze(0);
+
+	}
+
+	this.parse = function () {
+		var self = this;
+		$ = cheerio.load(this.body);
+		var links = $('a');
+		links.each( function( index, element ){
+			var label =  $(this).text().trim();
+			var link = $(this).attr('href');
+			self.links.push({label: label, link: link});
+		});
+	};
+
+	this.analyze = function (i) {
+		if( i < this.links.length ) {
+			var link = this.links[i].link;
+			var self = this;
+			request(link, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					self.links[i]['broken'] = false;
+				} else {
+					self.links[i]['broken'] = true;
+				}
+				self.analyze(i+1);
+			});
+		} else {
+			this.report();
+		}
+	};
+
+	this.parseUrl = function (url) {
+		var parts = url.parse(url, true);
+		if (parts.protocol === 'http:') {
+			return url;
+		} else if (parts.protocol === null) {
+			return this.url+url;
+		} else if (parts.protocol === 'javascript:') {
+			return url;
+		}
+	};
+
+	this.report = function () {
+		this.links.forEach( function(link) {
+			console.log(link.link+', '+link.broken);
+		}, this);
+	}
+
+	this.scrape = function () {
+		var self = this;
+		request(this.url, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				self.body = body;
+				self.process();
+			} else {
+				console.log(error);
+			}
+		});
+	};
+};
+
+var scraper = new Scraper();
+scraper.url = 'http://www.mobileacademy.com';
+scraper.scrape();
